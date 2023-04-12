ca046caf7c4130c0002a2f0dc8e03f9ef3167db9
Ahmad Kabani <ahmadkabani@yahoo.com>
2023-04-12 16:35:59 -0400

    customize inactive tab colors

--- chadrc.lua
+++ chadrc.lua
@@ -5,13 +5,21 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = 'ayu_dark',
+  theme = "ayu_dark",
+  theme_toggle = { "ayu_dark", "catppuccin" },
+
+  telescope = { style = "bordered" },
+
+  lsp_semantic_tokens = true,
+
   hl_override = {
     Comment = { fg = "#40e060" },
     LineNr = { fg = "#a0ffa0" },
     CursorLineNr = { fg = "#ff8080" },
     ColorColumn = { bg = "#303030" },
-  },
+    TbLineBufOn = { fg = "#ffffff" },
+    TbLineBufOff = { fg = "#b0b0b0" },
+  }
 }
 
 M.plugins = "custom.plugins"
