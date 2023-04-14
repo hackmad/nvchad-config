f95de95dab9971242c8308d436064254816ee372
Ahmad Kabani <ahmadkabani@yahoo.com>
2023-04-14 11:06:59 -0400

    format on save

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,19 +1,18 @@
-local present, null_ls = pcall(require, "null-ls")
-
-if not present then
-  return
-end
+local null_ls = require "null-ls"
 
 local sources = {
+  null_ls.builtins.formatting.clang_format,
   null_ls.builtins.formatting.prettier,
   null_ls.builtins.formatting.rustfmt,
   null_ls.builtins.formatting.scalafmt,
   null_ls.builtins.formatting.stylua,
 
   null_ls.builtins.code_actions.gitsigns,
+
+  null_ls.builtins.diagnostics.shellcheck,
 }
 
 null_ls.setup {
-   debug = true,
-   sources = sources,
+  debug = true,
+  sources = sources,
 }
--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 vim.wo.relativenumber = true
-vim.opt_global.shortmess:remove("F") -- See `:h nvim-metals`
+vim.opt_global.shortmess:remove "F" -- See `:h nvim-metals`
 vim.opt.colorcolumn = "120"
+
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = buffer,
+  callback = function()
+    vim.lsp.buf.format { async = false }
+  end,
+})
