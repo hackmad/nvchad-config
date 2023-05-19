3ad47b8d4b2365fe31d8e296cfd833b6dc20d1f5
Ahmad Kabani <ahmadkabani@yahoo.com>
2023-05-19 17:33:44 -0400

    configure fidget for nvim-metals

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,24 +3,50 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
+local handlers = {
+  ["metals/status"] = function(_, status, ctx)
+    -- https://github.com/scalameta/nvim-metals/blob/main/lua/metals/status.lua#L36-L50
+    local val = {}
+    if status.hide then
+      val = { kind = "end" }
+    elseif status.show then
+      val = { kind = "begin", message = status.text }
+    elseif status.text then
+      val = { kind = "report", message = status.text }
+    else
+      return
+    end
+    local info = { client_id = ctx.client_id }
+    local msg = { token = "metals", value = val }
+    -- call fidget progress handler
+    vim.lsp.handlers["$/progress"](nil, msg, info)
+  end,
+}
+
 local servers = {
-  cssls = {},
-  clangd = {},
-  dartls = {},
-  html = {},
-  gopls = {},
-  pyright = {},
-  rust_analyzer = {},
+  cssls = { on_attach = on_attach, capabilities = capabilities },
+  clangd = { on_attach = on_attach, capabilities = capabilities },
+  dartls = { on_attach = on_attach, capabilities = capabilities },
+  html = { on_attach = on_attach, capabilities = capabilities },
+  gopls = { on_attach = on_attach, capabilities = capabilities },
+  pyright = { on_attach = on_attach, capabilities = capabilities },
+  rust_analyzer = { on_attach = on_attach, capabilities = capabilities },
   terraformls = {
+    on_attach = on_attach,
+    capabilities = capabilities,
     filetypes = {
       "terraform",
       --"terraform-vars",
     },
   },
+  metals = {
+    init_options = {
+      statusBarProvider = "on",
+    },
+    handlers = handlers,
+  },
 }
 
 for lsp, cfg in pairs(servers) do
-  cfg.on_attach = on_attach
-  cfg.capabilities = capabilities
   lspconfig[lsp].setup(cfg)
 end
